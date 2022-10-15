51a608390288a4902c3c647e85eec20ec75e5bb5
olga <olga.matveeva.92@inbox.ru>
2022-10-16 00:45:12 +0800

    семинар 1

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿/*Задача 1. Напишите программу которая на вход принимает 2 числа и проверяет является ли 1 число квадратом второго.
+
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.Readline());
+
+int quad2 = n2 * n2;
+
+if (n1== quart2)
+{
+     Console.WriteLine("Yes");
+}
+else
+{
+     Console.WriteLine("No");
+}
+*/
+/*
+// Задача 2. Программа на вхлд принемает одно число н, а на выходе показывает от-н до н.
+
+Console.Write("Input a positiv integer mumber: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = -n;
+
+while(current <= n)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+/*
+///программа принемает на вход 3-значное число и показывает последнюю цифру числа.
+Console.Write("Input a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine ());
+
+int result = num % 10;
+Console.WriteLine ($"Lust digit of {num} is {result}");
+*/
